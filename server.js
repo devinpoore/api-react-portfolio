35096d13dccca9595ccea888f7507faab3324f3c
Dalinar Kholin <devinpoore@users.noreply.github.com>
2019-11-13 15:36:20 -0800

    enabled cors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 // const nodemailer = require("nodemailer");
 
 const app = express();
 
-// app.use(cors);
+app.use(cors);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
